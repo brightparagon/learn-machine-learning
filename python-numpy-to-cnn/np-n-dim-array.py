33b493221869d3b901d2e375f7df28b976c61ef3
노경모 <kyeongmo2@gmail.com>
2018-04-22 17:03:53 +0900

    n-dimension numpy array: access to element and logical operation broadcast

--- python-numpy-to-cnn/np-n-dim-array.py
+++ python-numpy-to-cnn/np-n-dim-array.py
@@ -14,3 +14,26 @@ print(A * B)
 # matrix * scala: broadcast
 print(A)
 print(A * 10)
+
+# broadcast in n-dim numpy array
+B = np.array([10, 20])
+print(A * B)
+
+# access to numpy array
+X = np.array([[51, 55], [14, 19], [0, 4]])
+print(X)
+print(X[0]) # first row of X
+print(X[0][1])
+
+# numpy array also can be accessed via loop
+for row in X:
+  print(row)
+
+# flatten numpy array
+X = X.flatten()
+print(X)
+print(X[np.array([0, 2, 4])])
+
+# logical operation broadcast
+print(X > 15)
+print(X[X > 15])
