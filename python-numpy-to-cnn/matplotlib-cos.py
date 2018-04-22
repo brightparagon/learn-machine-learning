9d7011ffca4485a093896e6123f6931666ce2b33
노경모 <kyeongmo2@gmail.com>
2018-04-23 00:16:11 +0900

    print cos graph using matplotlib

---
+++ python-numpy-to-cnn/matplotlib-cos.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib as mlp
+mlp.use('TkAgg')
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 6, 0.1)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+# graph
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos")
+plt.xlabel("x")
+plt.xlabel("y")
+plt.title("sin & cos")
+plt.legend()
+plt.show()
