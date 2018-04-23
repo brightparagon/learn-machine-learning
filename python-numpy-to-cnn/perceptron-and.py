4644e9a5aa2f2e35bafa1fd2384d8ffdf53576b2
노경모 <kyeongmo2@gmail.com>
2018-04-23 21:27:48 +0900

    deal with parameters and weights as numpy array and add bias to AND gate

--- python-numpy-to-cnn/perceptron-and.py
+++ python-numpy-to-cnn/perceptron-and.py
@@ -1,10 +1,18 @@
+import numpy as np
+
 def AND(x1, x2):
   # in this AND case actually theta would do its role when it's between 0.5, 0.999...
-  w1, w2, theta = 0.5, 0.5, 0.7
-  tmp = x1*w1 + x2*w2
-  if tmp <= theta:
+  # w1, w2, theta = 0.5, 0.5, 0.7
+  # tmp = x1*w1 + x2*w2
+  
+  x = np.array([x1, x2])
+  w = np.array([0.5, 0.5])
+  b = -0.7
+  tmp = np.sum(w*x) + b
+
+  if tmp <= 0:
     return 0
-  elif tmp > theta:
+  else:
     return 1
 
 print(AND(0, 0))
