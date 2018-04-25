40d262ad376f46ffa7e97967be49cd30665dec93
노경모 <kyeongmo2@gmail.com>
2018-04-25 23:45:25 +0900

    apply batch operation to predict function

--- python-numpy-to-cnn/17-start-mnist2.py
+++ python-numpy-to-cnn/17-start-mnist2.py
@@ -30,11 +30,13 @@ def predict(network, x):
 x, t = get_data()
 network = init_network()
 
+batch_size = 100
 accuracy_cnt = 0
-for i in range(len(x)):
-  y = predict(network, x[i])
-  p = np.argmax(y)
-  if p == t[i]:
-    accuracy_cnt += 1
+
+for i in range(0, len(x), batch_size):
+  x_batch = x[i: i+batch_size]
+  y_batch = predict(network, x_batch)
+  p = np.argmax(y_batch, axis=1)
+  accuracy_cnt += np.sum(p == t[i:i+batch_size])
 
 print("Accuracy: " + str(float(accuracy_cnt) / len(x)))
