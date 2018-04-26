748f84ea16ef56d38acc2f52c8ca65da04b0f79e
노경모 <kyeongmo2@gmail.com>
2018-04-27 00:26:09 +0900

    add comments to 17-start-mnist2

--- python-numpy-to-cnn/17-start-mnist2.py
+++ python-numpy-to-cnn/17-start-mnist2.py
@@ -18,6 +18,10 @@ def predict(network, x):
   W1, W2, W3 = network['W1'], network['W2'], network['W3']
   b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
+  # if x is one image
+  # (1, 784) * (784, 50) -> (1, 50) * (50, 100) -> (1, 100) * (100, 10) -> (1, 10)
+  # if x is 100 images
+  # (100, 784) * (784, 50) -> (100, 50) * (50, 100) -> (100, 100) * (100, 10) -> (100, 10)
   a1 = np.dot(x, W1) + b1
   z1 = sigmoid(a1)
   a2 = np.dot(z1, W2) + b2
@@ -34,9 +38,13 @@ batch_size = 100
 accuracy_cnt = 0
 
 for i in range(0, len(x), batch_size):
-  x_batch = x[i: i+batch_size]
-  y_batch = predict(network, x_batch)
-  p = np.argmax(y_batch, axis=1)
+  x_batch = x[i: i+batch_size] # 100, 784
+  y_batch = predict(network, x_batch) # 100, 10
+  p = np.argmax(y_batch, axis=1) # 100
+  if i == 0:
+    print(x_batch.shape, y_batch.shape, p.shape)
+    print(p == t[i:i+batch_size])
+    print(np.sum(p == t[i:i+batch_size]))
   accuracy_cnt += np.sum(p == t[i:i+batch_size])
 
 print("Accuracy: " + str(float(accuracy_cnt) / len(x)))
