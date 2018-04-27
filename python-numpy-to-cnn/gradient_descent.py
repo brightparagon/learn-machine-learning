212e7f5ca67f2f8fe799449655ea842cf2525e7d
노경모 <kyeongmo2@gmail.com>
2018-04-28 01:03:31 +0900

    rename numerical-diff.py and add gradient_descent function

---
+++ python-numpy-to-cnn/gradient_descent.py
@@ -0,0 +1,10 @@
+from numerical_diff import numerical_diff
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+  x = init_x
+
+  for i in range(step_num):
+    grad = numerical_diff(f, x)
+    x -= lr * grad
+
+  return x
