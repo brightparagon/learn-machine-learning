b2c8bbc7e6e8218d5d80a43e6e58e72480f293fe
노경모 <kyeongmo2@gmail.com>
2018-04-28 00:53:41 +0900

    modify numerical_diff function to make it able to calculate array data

--- python-numpy-to-cnn/numerical-diff.py
+++ python-numpy-to-cnn/numerical-diff.py
@@ -1,3 +1,17 @@
+import numpy as np
+
 def numerical_diff(f, x):
   h = 1e-4 # 0.0001
-  return (f(x + h) - f(x - h)) / (2 * h)
+  grad = np.zeros_like(x)
+
+  for idx in range(x.size):
+    tmp_val = x[idx]
+    x[idx] = tmp_val + h
+    fxh1 = f(x)
+    x[idx] = tmp_val - h
+    fxh2 = f(x)
+
+    grad[idx] = (fxh1 - fxh2) / (2 * h)
+    x[idx] = tmp_val
+
+  return grad
