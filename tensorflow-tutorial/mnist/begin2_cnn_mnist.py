5b1c1a6f58bbc4329f31a425b21e63bd7dafe021
노경모 <kyeongmo2@gmail.com>
2018-05-07 11:29:27 +0900

    add comments to begin2_cnn_mnist of tensorflow tutorial

--- tensorflow-tutorial/mnist/begin2_cnn_mnist.py
+++ tensorflow-tutorial/mnist/begin2_cnn_mnist.py
@@ -4,10 +4,14 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+# load mnist data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+# create an interactive session
 sess = tf.InteractiveSession()
 
+# image data(28 * 28)
 x = tf.placeholder(tf.float32, shape=[None, 784])
+# answer label
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
 # W = tf.Variable(tf.zeros([784,10]))
@@ -46,7 +50,7 @@ def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 # 1st conv layer
-W_conv1 = weight_variable([5, 5, 1, 32])
+W_conv1 = weight_variable([5, 5, 1, 32]) # 32 filters of 5 x 5 with 1 channel
 b_conv1 = bias_variable([32])
 
 x_image = tf.reshape(x, [-1,28,28,1])
@@ -55,7 +59,7 @@ h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 # 2nd conv layer
-W_conv2 = weight_variable([5, 5, 32, 64])
+W_conv2 = weight_variable([5, 5, 32, 64]) # 64 filters of 5 * 5 with 32 channels
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
@@ -72,7 +76,7 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-# last layey with softmax
+# last layer with softmax
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
 
@@ -84,6 +88,7 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.global_variables_initializer())
+
 for i in range(20000):
   batch = mnist.train.next_batch(50)
   if i%100 == 0:
